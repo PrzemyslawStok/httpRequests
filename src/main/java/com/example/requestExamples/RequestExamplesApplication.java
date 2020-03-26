1a0229a0b914c5570ae172a8445dc982dd32ddb9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-26 14:02:43 +0100

    gradle modification

--- src/main/java/com/example/requestExamples/RequestExamplesApplication.java
+++ src/main/java/com/example/requestExamples/RequestExamplesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RequestExamplesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RequestExamplesApplication.class, args);
 	}
 
