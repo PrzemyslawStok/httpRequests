0944ccbb10376c3aa8b9d205099114a5db688967
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-26 17:15:37 +0100

    function a*x+b

--- src/main/java/com/example/requestExamples/SimpleController.java
+++ src/main/java/com/example/requestExamples/SimpleController.java
@@ -1,13 +1,28 @@
 package com.example.requestExamples;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SimpleController {
-    @RequestMapping("/")
-    @ResponseBody String getInfo(){
+    @GetMapping("/")
+    @ResponseBody
+    String getInfo(){
         return "Info...";
+    }
+    @RequestMapping("f0")
+    @ResponseBody
+    String getValue(@RequestParam("x") Double x,
+                    @RequestParam(value = "a",required=false) Double a,
+                    @RequestParam(value = "b",required=false) Double b){
+        //f(x)=a*x+b
+        if(a==null)
+            return "A nie zostało określone";
+        if(b==null)
+            return "B nie zostało określone";
+
+        Double f0 = a*x+b;
+
+        return f0.toString();
     }
 }
