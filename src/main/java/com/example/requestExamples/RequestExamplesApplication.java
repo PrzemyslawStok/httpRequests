c4dbdc4caf1fea833da6bc4deece5d41d47f1248
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-26 13:30:52 +0100

    first commit

---
+++ src/main/java/com/example/requestExamples/RequestExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestExamplesApplication.class, args);
+	}
+
+}
